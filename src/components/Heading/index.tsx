b0310ae07f3872eba3b90be7b35bffb5de00c603
Wellington Nascimento <wellingto_07@hotmail.com>
2022-10-25 13:55:39 -0300

    feat: create heading, story, layout e, styles and test

---
+++ src/components/Heading/index.tsx
@@ -0,0 +1,21 @@
+import * as S from './styles'
+
+export type HeadingProps = {
+  children: React.ReactNode
+  color?: 'white' | 'black'
+  lineLeft?: boolean
+  lineBottom?: boolean
+}
+
+const Heading = ({
+  children,
+  color = 'white',
+  lineLeft = false,
+  lineBottom = false
+}: HeadingProps) => (
+  <S.Wrapper color={color} lineBottom={lineBottom} lineLeft={lineLeft}>
+    {children}
+  </S.Wrapper>
+)
+
+export default Heading
