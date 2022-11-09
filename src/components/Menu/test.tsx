0840fe1231ab0be14c82cbb6dca992e03e024899
Wellington Nascimento <wellingto_07@hotmail.com>
2022-11-08 21:42:01 -0300

    feat: create tast unit menu

---
+++ src/components/Menu/test.tsx
@@ -0,0 +1,18 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Menu from '.'
+
+describe('<Menu />', () => {
+  it('should render the menu', () => {
+    renderWithTheme(<Menu />)
+    expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/search/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/open shopping cart/i)).toBeInTheDocument()
+  })
+
+  it('should render the logo', () => {
+    renderWithTheme(<Menu />)
+    expect(screen.getByLabelText(/won games/i)).toBeInTheDocument()
+  })
+})
