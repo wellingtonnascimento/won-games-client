e60843a2b8227b220167050be9daa48fefa751f5
Wellington Nascimento <wellingto_07@hotmail.com>
2022-11-29 20:45:00 -0300

    feat: add line color, teste, type btn

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -1,19 +1,31 @@
 import * as S from './styles'
 
+export type LineColors = 'primary' | 'secondary'
+
 export type HeadingProps = {
   children: React.ReactNode
   color?: 'white' | 'black'
   lineLeft?: boolean
   lineBottom?: boolean
+  lineColor?: LineColors
+  size?: 'small' | 'medium'
 }
 
 const Heading = ({
   children,
   color = 'white',
   lineLeft = false,
-  lineBottom = false
+  lineBottom = false,
+  lineColor = 'primary',
+  size = 'medium'
 }: HeadingProps) => (
-  <S.Wrapper color={color} lineBottom={lineBottom} lineLeft={lineLeft}>
+  <S.Wrapper
+    color={color}
+    lineBottom={lineBottom}
+    lineLeft={lineLeft}
+    lineColor={lineColor}
+    size={size}
+  >
     {children}
   </S.Wrapper>
 )
