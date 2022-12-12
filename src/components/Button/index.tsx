9c34802d12c382ac2176b56dd99ef33935abe46b
Wellington Nascimento <wellingto_07@hotmail.com>
2022-12-12 14:52:48 -0300

    feat: add polimorfismo no btn

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,12 +1,16 @@
+import { AnchorHTMLAttributes } from 'react'
 import * as S from './styles'
 
+type ButtonTypes =
+  | AnchorHTMLAttributes<HTMLAnchorElement>
+  | AnchorHTMLAttributes<HTMLButtonElement>
+
 export type ButtonProps = {
-  children?: React.ReactNode
   size?: 'small' | 'medium' | 'large'
   fullWidth?: boolean
   icon?: React.ReactNode
-  onClick?: () => (event: React.MouseEvent<HTMLButtonElement>) => void
-}
+  as?: React.ElementType
+} & ButtonTypes
 
 const Button = ({
   children,
