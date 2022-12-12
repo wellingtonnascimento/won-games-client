bd351b7059e5ef9d39a62004e7b917f015bac67a
Wellington Nascimento <wellingto_07@hotmail.com>
2022-12-12 15:34:39 -0300

    feat: create componente banner stratuc

---
+++ src/components/Banner/test.tsx
@@ -0,0 +1,12 @@
+// import { render, screen } from '@testing-library/react'
+
+// import Banner from '.'
+
+describe('<Banner />', () => {
+  it('should render correctly', () => {
+    // verifique se o title existe renderizado
+    // verifique se o subtitle existe renderizado
+    // verifique se a imagem existe renderizado
+    // verifique se o button existe renderizado
+  })
+})
