de9e1a602c00b14c465b66b2ac44f53f8c5f37d5
Wellington Nascimento <wellingto_07@hotmail.com>
2022-12-20 17:06:39 -0300

    feat: create teste

--- src/components/Banner/test.tsx
+++ src/components/Banner/test.tsx
@@ -1,12 +1,28 @@
-// import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
-// import Banner from '.'
+import Banner from '.'
+
+const props = {
+  img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+  title: 'Defy death',
+  subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+  buttonLabel: 'Buy now',
+  buttonLink: '/games/defy-death'
+}
 
 describe('<Banner />', () => {
   it('should render correctly', () => {
-    // verifique se o title existe renderizado
-    // verifique se o subtitle existe renderizado
-    // verifique se a imagem existe renderizado
-    // verifique se o button existe renderizado
+    renderWithTheme(<Banner {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: /defy death/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /Play the new CrashLands season/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('img', { name: /Defy death/i })).toBeInTheDocument()
   })
 })
